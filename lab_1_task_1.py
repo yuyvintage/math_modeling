425b5c46c34cf9f5bab2faaf8e186904398b61f9
yuyvintage <137422227+yuyvintage@users.noreply.github.com>
2023-06-27 14:27:11 +0000

    pervoe zadanie

---
+++ lab_1_task_1.py
@@ -0,0 +1,5 @@
+print('Hello, world!')
+print('Маша + Петя = любовь')
+print('x = 3 + 4')
+x = 3 + 4
+print(f'x = {x}')
---
+++ lab_1_task_2.py
@@ -0,0 +1,4 @@
+print(type('Hello, world!'))
+print(type(3 + 4))
+print(type(3 / 4))
+print(type([1, 2, 5, 10, 100]))
