cbd79b8553f751898ef85191b7e1b79cfd351a7c
yuyvintage <137422227+yuyvintage@users.noreply.github.com>
2023-07-17 13:46:28 +0000

    21213

--- lab_3_doptask_1.py
+++ lab_3_doptask_1.py
@@ -16,6 +16,20 @@ for i in range(0, 4, 1):
     for j in range(0, 3, 1):
         B[i, j] = int(input('напишите число:'))
 
+print(B)
+
+C = np.ones((4, 3))
+
+for i in range(0, 4, 1):
+    for j in range(0, 3, 1):
+        if A[i, j] > B[i,j]:
+            C[i,j] = A[i, j]
+    elif:
+         C[i,j] = B[i, j]
+      
+print(C)
+
+
 
 
 
