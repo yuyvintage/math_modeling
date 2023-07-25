7f3d4088f7d4fe6a09156f8335cf5dffe91a71c5
yuyvintage <137422227+yuyvintage@users.noreply.github.com>
2023-07-25 12:50:30 +0000

    213312

---
+++ lab_4_task_4.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+a = int(input('укажите промежуток функции:'))
+b = int(input('укажите промежуток функции:'))
+N = int(input('укажите точки функции:'))
+
+def function(a, b):
+    y = N
+    y = x**2
+    a < x < b
+    return x
+
+print(function)
